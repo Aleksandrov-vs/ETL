2287849712d93c9e310e90e0690c92a4e3de2ad7
vasya <dfcz200115@mail.ru>
2023-04-12 00:59:57 +0600

    add transformer

---
+++ etl/postgres_to_es/transformer.py
@@ -0,0 +1,43 @@
+from typing import Dict, List
+
+from psycopg2.extras import RealDictRow
+
+from models import Actor, Filmwork, Writer
+
+
+class DataTransform:
+
+    def transform_persons(self, persons: List[Dict]):
+        writer_list: List[Writer] = []
+        actor_list: List[Actor] = []
+        director_list: List[str] = []
+
+        for p in persons:
+            if p['person_role'] == 'writer':
+                writer_list.append(
+                    Writer(id=p['person_id'], name=p['person_name'])
+                )
+            if p['person_role'] == 'actor':
+                actor_list.append(
+                    Actor(id=p['person_id'], name=p['person_name'])
+                )
+            if p['person_role'] == 'director':
+                director_list.append(p['person_name'])
+
+        return writer_list, actor_list, director_list
+
+    def transform_data(self, data: List[RealDictRow]) -> List[Filmwork]:
+        film_works = []
+        for row in data:
+            writer_list, actor_list, director_list = self.transform_persons(
+                row.pop('persons')
+            )
+            genres = row.pop('genres')
+            film_work = Filmwork(
+                **row, actors=actor_list,
+                writers=writer_list,
+                director=director_list,
+                genre=genres
+            )
+            film_works.append(film_work)
+        return film_works
